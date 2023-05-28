8ea081a6a8b7a982c2ea2dafb8bb95d3f70a3f12
Elkott1 <103424889+Elkott1@users.noreply.github.com>
2023-05-28 17:42:30 +0300

    admin reports

--- bankingapp/src/Admin/Reports/Reports.js
+++ bankingapp/src/Admin/Reports/Reports.js
@@ -1,11 +1,19 @@
 
+import TechnicalIssueTable from "../../Components/ TechnicalIssueTable";
+import MenuBar from "../../Components/MenuBar";
 
 function Reports(){
 
     return(
+       
+        
+    
         <div>
+            <MenuBar />
+            <TechnicalIssueTable/>
 
         </div>
+        
 
     );
 }
