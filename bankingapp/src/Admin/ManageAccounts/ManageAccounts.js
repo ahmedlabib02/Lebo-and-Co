4900bddf071d8337d11ad1cba82c04ad5421e7a6
A-Badawi <Ahmad.badawi369@gmail.com>
2023-05-29 01:23:34 +0300

    Anything

--- bankingapp/src/Admin/ManageAccounts/ManageAccounts.js
+++ bankingapp/src/Admin/ManageAccounts/ManageAccounts.js
@@ -1,4 +1,5 @@
 import ButtonCard from "../../Components/ButtonCard";
+import React, { useState } from "react";
 import useNavigation from '../../hooks/use-navigation';
 import MenuBar from "../../Components/MenuBar";
 import Footer from "../../Components/Footer";
@@ -8,12 +9,25 @@ import { CiViewList } from 'react-icons/ci';
 import ManageAccountTable from "../../Components/ManageAccountTable";
 import AdminMenuBar from "../AdminMenuBar";
 function ManageAccounts(){
- return(
-  <div>
-  <AdminMenuBar/>
-  <ManageAccountTable/>
-  </div>
+    const { navigate } = useNavigation();
+  const [searchQuery, setSearchQuery] = useState("");
+  const [filteredUsers, setFilteredUsers] = useState([]);
 
- )
+  // Function to handle search query change
+  const handleSearchQueryChange = (event) => {
+    const query = event.target.value;
+    setSearchQuery(query);
+  }
+ return(
+    <div>
+    <AdminMenuBar />
+    <div>
+      <div className="flex justify-center">
+        
+      </div>
+      <ManageAccountTable searchQuery={searchQuery} />
+    </div>
+  </div>
+);
 }
 export default ManageAccounts;
